97ec7c6d3cee34625cabefad01d5051b7765fb83
Amador Santiago <amadorsantiago368@gmail.com>
2022-03-30 14:22:58 -0600

    configured files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -44,7 +44,8 @@ export default defineComponent({
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped  lang="scss">
+$base-color: #c6538c;
 h3 {
   margin: 40px 0 0;
 }
